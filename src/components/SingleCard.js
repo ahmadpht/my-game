c2a4f88c10a6a05ddbfb8652f5c3ceab95bc57b8
ahmadpht <ahmadpirhayati7777@gmail.com>
2023-12-21 13:15:06 +0330

    my game

---
+++ src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import './SingleCard.css'
+
+export default function SingleCard({card, handleChoice, flipped, disabled}) {
+
+    const handleClick = () => {
+        if(!disabled){
+            handleChoice(card)
+        }
+    }
+
+    return (
+        <div className='card'>
+            <div className={flipped ? "flipped":"s"}>
+                <img className='front' src={card.src} alt="card front" />
+                <img
+                    className='back'
+                    src="/images/7.jpg"
+                    onClick={handleClick}
+                    alt="card back"
+                />
+            </div>
+        </div>
+    )
+}
